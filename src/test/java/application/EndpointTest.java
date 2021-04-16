a9bdd4bb985f14b1d906dfca591ee30fed8d56c0
Youming Lin <ylin@Youmings-MacBook-Pro.local>
2021-04-16 10:12:25 -0500

    Update to JUnit 5

--- src/test/java/application/EndpointTest.java
+++ src/test/java/application/EndpointTest.java
@@ -1,10 +1,9 @@
 package application;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -12,24 +11,23 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class EndpointTest {
 
-    @Autowired
-    private TestRestTemplate server;
-    
-    @LocalServerPort
-    private int port;
+	@Autowired
+	private TestRestTemplate server;
 
-    @Test
-    public void testEndpoint() throws Exception {
-        String endpoint = "http://localhost:" + port;
-        ResponseEntity<String> response = server.getForEntity(endpoint, String.class);
-        HttpStatus status = response.getStatusCode();
-        assertEquals("Invalid response from server : " + response, HttpStatus.OK, status);
-    }
+	@LocalServerPort
+	private int port;
 
+	@Test
+	public void testEndpoint() throws Exception {
+		String endpoint = "http://localhost:" + port;
+		ResponseEntity<String> response = server.getForEntity(endpoint, String.class);
+		HttpStatus status = response.getStatusCode();
+		assertEquals(HttpStatus.OK, status, "Invalid response from server : " + response);
+	}
 }
