a717374946dbe5da08c9a84bb162ad195a3711a5
Ellen Wang <Ellen.Wang@ibm.com>
2021-06-28 10:43:38 -0400

    Cleaned up imports

--- src/main/java/application/SBApplication.java
+++ src/main/java/application/SBApplication.java
@@ -1,22 +1,11 @@
 package application;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springdoc.core.GroupedOpenApi;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 
-
-@Configuration
 @SpringBootApplication
 public class SBApplication {
     @Bean
