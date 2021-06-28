dee9109411bfaf2eb79a8f6763101cc1e0903337
Ellen Wang <Ellen.Wang@ibm.com>
2021-06-28 12:27:37 -0400

    Fix spaces?

--- src/main/java/application/SBApplication.java
+++ src/main/java/application/SBApplication.java
@@ -9,12 +9,12 @@ import org.springdoc.core.GroupedOpenApi;
 @SpringBootApplication
 public class SBApplication {
     @Bean
-	public GroupedOpenApi actuatorOpenApi() {
-		return GroupedOpenApi.builder()
-				.group("api")
-				.pathsToMatch("/health", "/v1")
-				.build();
-	}
+    public GroupedOpenApi actuatorOpenApi() {
+	    return GroupedOpenApi.builder()
+		    .group("api")
+		    .pathsToMatch("/health", "/v1")
+		    .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SBApplication.class, args);
